fa03543e36995222c1a1352fc419fb61ce564007
David Purrington <dpurrington@cimpress.com>
2016-06-14 14:24:06 -0400

    Removing restriction to use environment variables because this is not a requirement of the sdk

--- lib/lambda_wrap/aws_setup.rb
+++ lib/lambda_wrap/aws_setup.rb
@@ -12,15 +12,10 @@ module LambdaWrap
     # to the AWS SDK
     #
     # *Required environment variables*
-    # [AWS_ACCESS_KEY_ID]       The AWS access key to be used when calling AWS SDK functions
-    # [AWS_SECRET_ACCESS_KEY]   The AWS secret to be used when calling AWS SDK functions
     # [AWS_REGION]              A valid AWS region to use during configuration.
     def validate
       # validate settings
-      raise 'AWS_ACCESS_KEY_ID not set' unless ENV['AWS_ACCESS_KEY_ID']
-      raise 'AWS_SECRET_ACCESS_KEY not set' unless ENV['AWS_SECRET_ACCESS_KEY']
       raise 'AWS_REGION not set' unless ENV['AWS_REGION']
-
       Aws.use_bundled_cert!
     end
   end
