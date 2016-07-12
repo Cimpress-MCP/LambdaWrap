fa75e765debd0e2d802cb6553c471280c2c0d8e6
Zaid Momin <zmomin@cimpress.com>
2016-07-12 19:05:00 +0530

    Added the vpc configuration check.

--- lib/lambda_wrap/lambda_manager.rb
+++ lib/lambda_wrap/lambda_manager.rb
@@ -89,12 +89,20 @@ module LambdaWrap
         func_version = func_config.version
         raise 'Error while publishing existing lambda function ' + function_name unless func_version
       rescue Aws::Lambda::Errors::ResourceNotFoundException
+        # check if vpc_subnet_ids and vpc_security_group_ids are empty and set accordingly to null
+        vpc_Configuration= []
+        if (vpc_subnet_ids.empty? && vpc_security_group_ids.empty?)
+            vpc_Configuration = nil
+        else
+            vpc_Configuration = { subnet_ids: vpc_subnet_ids, security_group_ids: vpc_security_group_ids }
+        end
+
         # if we cannot find it, we have to create it instead of updating it
         func_config = @client.create_function(
           function_name: function_name, runtime: 'nodejs4.3', role: lambda_role,
           handler: handler, code: { s3_bucket: bucket, s3_key: key }, timeout: 5, memory_size: 128, publish: true,
           description: lambda_description,
-          vpc_config: { subnet_ids: vpc_subnet_ids, security_group_ids: vpc_security_group_ids }
+          vpc_config: vpc_Configuration
         ).data
         puts func_config
         func_version = func_config.version
